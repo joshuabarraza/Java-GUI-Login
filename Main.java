a8562db0b29424c5ab97d44ad2e00a6045baad93
Josh Barraza <100009893+joshuabarraza@users.noreply.github.com>
2022-12-02 00:01:32 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,8 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		//LoginPage login = new LoginPage();
-		MainWindow window = new MainWindow();
+		LoginPage login = new LoginPage();
+		//MainWindow window = new MainWindow();		<- when testing, you can assess the Main Window directly
 	}
 
 }
